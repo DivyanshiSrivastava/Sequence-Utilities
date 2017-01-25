91245207b608fcc5725e4f8d51c1f44c848f7a5e
dvs5680 <s.divyanshi91@gmail.com>
2017-01-25 12:37:56 -0500

    adding functions

--- wildcard_kmers.py
+++ wildcard_kmers.py
@@ -16,16 +16,30 @@ def prune(seq):
 
 def all_kmers():
 	# get a set of all possible strings from this alphabet.
-	space = list(set(itertools.product(('A','T','G','C','N','N'), repeat = 6)))
+	space = list(set(itertools.product(('A','T','G','C','N','N'), repeat = 8)))
 	subset = np.array([prune(space[i]) for i in range(0,len(space))])
 	return np.asarray(space)[subset]
 
 """ getting sequence specific kmers """
+
+def add_wildcards(kmer):
+	#  addinf wildcards to a kmes.	
+	wildcard = []
+	for i in range(0,len(kmer)-1):
+		hold = kmer.copy()
+		hold[i] = 'N'
+		hold[i + 1] = 'N'	
+		wildcard.append(list(hold))
+	return np.array(wildcard)
+
 def get_kmers(seq):
 	# 6 & 5 hardcoded for 6 mers. Remove that if making it not rough.
 	# the array and list is to get it in the same format as the unversal set. 
-	kmers = np.asarray([list(seq[i:i+6]) for i in range(0,len(seq)-5)])
-	return kmers
+	kmers = np.asarray([list(seq[i:i+8]) for i in range(0,len(seq)-7)])
+	wkmers = np.empty((0,8), str)
+	for kmer in kmers:
+		wkmers = np.vstack((wkmers,add_wildcards(kmer)))
+	print wkmers
 
-features = get_kmers('ATGCGCTAGCG')
-space = all_kmers()
+get_kmers('ATGCGCTGC')
+print all_kmers()
