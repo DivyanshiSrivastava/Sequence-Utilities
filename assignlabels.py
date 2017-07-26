666e45a29a3f6c200b12a16dd6c1a2dd592905f4
DIVYANSHI SRIVASTAVA <s.divyanshi91@gmail.com>
2017-07-26 13:57:46 -0400

    updating label assignment

--- assignlabels.py
+++ assignlabels.py
@@ -6,13 +6,13 @@ refined_labels = labels.copy()
 
 for ids in np.argwhere(labels=="SB"):
 	if labels[int(ids)-1] != "SB":
-		refined_labels[int(ids) -1] ="A"
+		refined_labels[int(ids)-3:int(ids)] ="A"
 	if labels[int(ids)+1] != "SB":
-		refined_labels[int(ids) +1] ="A"
+		refined_labels[int(ids)+1:int(ids)+4] ="A"
 	else:
 		pass
 
 for ids in np.argwhere(refined_labels =="WB"):
-	refined_labels[int(ids)-1: int(ids)+2] = "A"
+	refined_labels[int(ids)-3: int(ids)+4] = "A"
 
 np.savetxt("Ascl1.labels", refined_labels, fmt = "%s")
